d28bfada0cb6f7e7b4205c73937e7cec7a7942b4
ArturoAlcorta <145702372+ArturoAlcorta@users.noreply.github.com>
2026-09-09 23:21:44 +0200

    update db for profile use

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime, Float, String, func
+from sqlalchemy import DateTime, Float, String, func, Bool
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -19,4 +19,6 @@ class Run(Base):
     status: Mapped[str] = mapped_column(String, nullable=False, default="pending")
     celery_task_id: Mapped[str | None] = mapped_column(String, nullable=True)
     video_filename: Mapped[str] = mapped_column(String, nullable=False)
+    profile_used: Mapped[bool] = mapped_column(Bool, nullable=True)
+    calculated_1rm: Mapped[float] = mapped_column(Bool, nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
